1168172a165bbaf3246a6096ddef4ed4dec3bb4b
Filip Štěpánek <stepafil@gmail.com>
2022-09-18 17:06:47 +0200

    Revert "Test removal of assignment pages"
    This reverts commit ae1151988e0f3a63a419f707f0d529886985645d.

---
+++ pages/assignments/new.tsx
@@ -0,0 +1,13 @@
+import {NextPage} from "next";
+import AssignmentEditor from "../../components/assignments/AssignmentEditor";
+
+
+const New: NextPage = () => {
+
+    return (
+        <AssignmentEditor/>
+    )
+
+}
+
+export default New

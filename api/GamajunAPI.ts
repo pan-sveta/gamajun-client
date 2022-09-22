a853d6ae5d9922e12e997979af172693dee8719f
Filip Štěpánek <stepafil@gmail.com>
2022-09-22 23:30:29 +0200

    Admin check fix

--- api/GamajunAPI.ts
+++ api/GamajunAPI.ts
@@ -22,21 +22,21 @@ const gamajunHeaders = async (token: string): Promise<Record<string, string>> =>
 // HTTP Methods
 
 const get = async (endpoint: string, token: string): Promise<Response> => {
-    return fetch(`https://gamajun-api.stepanek.app/${endpoint}`, {
+    return fetch(`https://gamajun-api.stepanek.app${endpoint}`, {
         method: "GET",
         headers: await gamajunHeaders(token),
     })
 }
 
 const del = async (endpoint: string, token: string): Promise<Response> => {
-    return fetch(`https://gamajun-api.stepanek.app/${endpoint}`, {
+    return fetch(`https://gamajun-api.stepanek.app${endpoint}`, {
         method: "DELETE",
         headers: await gamajunHeaders(token),
     })
 }
 
 const post = async (endpoint: string, body: any, token: string): Promise<Response> => {
-    return fetch(`https://gamajun-api.stepanek.app/${endpoint}`, {
+    return fetch(`https://gamajun-api.stepanek.app${endpoint}`, {
         method: "POST",
         headers: await gamajunHeaders(token),
         body: body
@@ -44,7 +44,7 @@ const post = async (endpoint: string, body: any, token: string): Promise<Respons
 }
 
 const put = async (endpoint: string, body: any, token: string): Promise<Response> => {
-    return fetch(`https://gamajun-api.stepanek.app/${endpoint}`, {
+    return fetch(`https://gamajun-api.stepanek.app${endpoint}`, {
         method: "PUT",
         headers: await gamajunHeaders(token),
         body: body
@@ -75,92 +75,92 @@ const checkIfOk = (res: Response): void => {
 //Assignments
 
 export const createAssignment = async (assignment: Assignment, token: string): Promise<Assignment> => {
-    return post("https://gamajun-api.stepanek.app/assignments", token, JSON.stringify(assignment))
+    return post("/assignments", token, JSON.stringify(assignment))
         .then(res => jsonIfOk(res))
         .then(json => AssignmentFromJSON(json));
 }
 
 export const getAssignment = async (id: string, token: string): Promise<Assignment> => {
-    return get(`https://gamajun-api.stepanek.app/assignments/${id}`, token)
+    return get(`/assignments/${id}`, token)
         .then(res => jsonIfOk(res))
         .then(json => AssignmentFromJSON(json));
 }
 
 export const getAllAssignments = async (token: string): Promise<Array<Assignment>> => {
-    return get("https://gamajun-api.stepanek.app/assignments", token)
+    return get("/assignments", token)
         .then(res => jsonArrayIfOk(res))
         .then(arr => arr.map((ass) => AssignmentFromJSON(ass)));
 }
 
 export const updateAssignment = async (assignment: Assignment, token: string): Promise<Assignment> => {
-    return put(`https://gamajun-api.stepanek.app/assignments/${assignment.id}`, JSON.stringify(assignment), token)
+    return put(`/assignments/${assignment.id}`, JSON.stringify(assignment), token)
         .then(res => jsonIfOk(res))
         .then(json => AssignmentFromJSON(json));
 }
 
 export const deleteAssignment = async (assignmentId: string, token: string): Promise<void> => {
-    return del(`https://gamajun-api.stepanek.app/assignments/${assignmentId}`, token)
+    return del(`/assignments/${assignmentId}`, token)
         .then(res => checkIfOk(res))
 }
 
 //Exams
 
 export const createExam = async (exam: Exam, token: string): Promise<Exam> => {
-    return put("https://gamajun-api.stepanek.app/exams", JSON.stringify(exam), token)
+    return put("/exams", JSON.stringify(exam), token)
         .then(res => jsonIfOk(res))
         .then(json => ExamFromJSON(json));
 }
 
 export const getAllExams = async (token: string): Promise<Array<any>> => {
-    return get("https://gamajun-api.stepanek.app/exams", token)
+    return get("/exams", token)
         .then(res => jsonArrayIfOk(res))
     //.then(arr => arr.map((ass) => ExamFromJSON(ass)));
 }
 
 export const getExam = async (id: string, token: string): Promise<any> => {
-    return get(`https://gamajun-api.stepanek.app/exams/${id}`, token)
+    return get(`/exams/${id}`, token)
         .then(res => jsonIfOk(res))
     //.then(json => ExamFromJSON(json));
 }
 
 export const updateExam = async (exam: Exam, token: string): Promise<Exam> => {
-    return put(`https://gamajun-api.stepanek.app/exams/${exam.id}`, JSON.stringify(exam), token)
+    return put(`/exams/${exam.id}`, JSON.stringify(exam), token)
         .then(res => jsonIfOk(res))
         .then(json => ExamFromJSON(json));
 }
 
 export const deleteExam = async (examId: string, token: string): Promise<void> => {
-    return del(`https://gamajun-api.stepanek.app/exams/${examId}`, token)
+    return del(`/exams/${examId}`, token)
         .then(res => checkIfOk(res))
 }
 
 //Admin
 
 export const createAdmin = async (admin: Admin, token: string): Promise<Admin> => {
-    return put("https://gamajun-api.stepanek.app/admins", JSON.stringify(admin), token)
+    return put("/admins", JSON.stringify(admin), token)
         .then(res => jsonIfOk(res))
         .then(json => AdminFromJSON(json));
 }
 
 export const getAllAdmins = async (token: string): Promise<Array<Admin>> => {
-    return get("https://gamajun-api.stepanek.app/admins", token)
+    return get("/admins", token)
         .then(res => jsonArrayIfOk(res))
         .then(arr => arr.map((ass) => AdminFromJSON(ass)));
 }
 
 export const getAdmin = async (username: string, token: string): Promise<Admin> => {
-    return get(`https://gamajun-api.stepanek.app/admins/${username}`, token)
+    return get(`/admins/${username}`, token)
         .then(res => jsonIfOk(res))
         .then(json => AdminFromJSON(json));
 }
 
 export const updateAdmin = async (admin: Admin, token: string): Promise<Admin> => {
-    return put(`https://gamajun-api.stepanek.app/admins/${admin.username}`, JSON.stringify(admin), token)
+    return put(`/admins/${admin.username}`, JSON.stringify(admin), token)
         .then(res => jsonIfOk(res))
         .then(json => AdminFromJSON(json));
 }
 
 export const deleteAdmin = async (adminId: string, token: string): Promise<void> => {
-    return del(`https://gamajun-api.stepanek.app/admins/${adminId}`, token)
+    return del(`/admins/${adminId}`, token)
         .then(res => checkIfOk(res))
 }
--- services/admin.ts
+++ services/admin.ts
@@ -3,7 +3,7 @@ import {GamajunApiError} from "../api/GamajunApiError";
 
 export const isUserAdmin = async (username: string, token: string): Promise<boolean> => {
     try {
-        let admin = getAdmin(username, token);
+        let admin = await getAdmin(username, token);
         return true;
     } catch (err) {
         if (err instanceof GamajunApiError) {

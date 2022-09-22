be0ea2b6e0d3c397c2d634e52419a1388a04aa94
Filip Štěpánek <stepafil@gmail.com>
2022-09-22 23:17:40 +0200

    Audio test rollback

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -3,19 +3,12 @@ import {usePageLeave} from "@mantine/hooks";
 import {useState} from "react";
 
 const Settings: NextPage = () => {
-    const audio = new Audio("http://wolo-usa.com/365c-stereo.wav");
-    const [leftsCount, setLeftsCount] = useState(0);
-    function handlePageLeave() {
-        audio.play().then();
-        setLeftsCount((p)=>p+1)
-    }
 
-    usePageLeave(() => handlePageLeave());
+
 
     return (
         <div>
             <h1>Nastavení</h1>
-            {leftsCount}
         </div>
     )
 }

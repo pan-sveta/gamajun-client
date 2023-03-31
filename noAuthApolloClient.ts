79e64f6c3bffb5187d6f73d33780547537f18472
Filip Štěpánek <stepafil@gmail.com>
2023-03-31 13:47:54 +0200

    Commiot

--- noAuthApolloClient.ts
+++ noAuthApolloClient.ts
@@ -4,6 +4,7 @@ const httpLink = createHttpLink({
     uri: `${process.env.NEXT_PUBLIC_GAMAJUN_API_URL}/graphql`,
 });
 
+
 export const gamajunApolloClient = new ApolloClient({
     link: httpLink,
     cache: new InMemoryCache(),
